c9c9cdb2a18373154ef1de6566638c7d90559ccc
Marcos Pablo <marquin.pablo@gmail.com>
2026-02-20 16:49:11 -0300

    feat: save thumbnails to disk and update video metadata
    Refactor thumbnail upload handler to store uploaded files on disk using a
    generated asset path. Update video metadata to reference the local asset URL
    instead of a base64 data URL. Add utility functions for asset path and URL
    generation.

--- handler_upload_thumbnail.go
+++ handler_upload_thumbnail.go
@@ -1,10 +1,10 @@
 package main
 
 import (
-	"encoding/base64"
 	"fmt"
 	"io"
 	"net/http"
+	"os"
 
 	"github.com/bootdotdev/learn-file-storage-s3-golang-starter/internal/auth"
 	"github.com/google/uuid"
@@ -56,13 +56,6 @@ func (cfg *apiConfig) handlerUploadThumbnail(w http.ResponseWriter, r *http.Requ
 		return
 	}
 
-	data, err := io.ReadAll(file)
-
-	if err != nil {
-		respondWithError(w, http.StatusInternalServerError, "Unable read file content", err)
-		return
-	}
-
 	video, err := cfg.db.GetVideo(videoID)
 
 	if video.UserID != userID {
@@ -75,10 +68,25 @@ func (cfg *apiConfig) handlerUploadThumbnail(w http.ResponseWriter, r *http.Requ
 		return
 	}
 
-	base64Encoded := base64.StdEncoding.EncodeToString(data)
+	assetPath := getAssetPath(videoID, mediaType)
+	assetDiskPath := cfg.getAssetDiskPath(assetPath)
 
-	base64DataURL := fmt.Sprintf("data:%s;base64,%s", mediaType, base64Encoded)
-	video.ThumbnailURL = &base64DataURL
+	dst, err := os.Create(assetDiskPath)
+
+	if err != nil {
+		respondWithError(w, http.StatusInternalServerError, "Couldn't create file", err)
+		return
+	}
+
+	defer dst.Close()
+
+	if _, err = io.Copy(dst, file); err != nil {
+		respondWithError(w, http.StatusInternalServerError, "Couldn't write to file", err)
+		return
+	}
+
+	thumbURL := cfg.getAssetURL(assetPath)
+	video.ThumbnailURL = &thumbURL
 
 	err = cfg.db.UpdateVideo(video)
 
